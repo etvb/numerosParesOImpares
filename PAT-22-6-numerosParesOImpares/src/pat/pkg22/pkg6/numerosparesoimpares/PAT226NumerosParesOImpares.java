f26d572890376ff0103470d51055e7c657c7d5c1
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-09-20 19:36:21 -0500

    archivos base

---
+++ PAT-22-6-numerosParesOImpares/src/pat/pkg22/pkg6/numerosparesoimpares/PAT226NumerosParesOImpares.java
@@ -0,0 +1,22 @@
+/*
+* HAcer un programa que tome dos números y digan si ambos son pares o impares.
+*/
+package pat.pkg22.pkg6.numerosparesoimpares;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author edgardo
+ */
+public class PAT226NumerosParesOImpares {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       
+    }
+    
+}
